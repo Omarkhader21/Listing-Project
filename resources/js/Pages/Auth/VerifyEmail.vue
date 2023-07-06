acac2153bde5d92e12421f4139d6c438d9dfffd1
Omarkhader21 <omar.khaderj21@gmail.com>
2023-07-07 00:02:39 +0300

    Email Update

---
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,11 @@
+<template>
+        You need to be verified first to see this page!
+
+    <div>
+        <Link :href="route('verification.send')" method="post" as="button" class="text-indigo-600 dark:text-indigo-400 ">Resend Verification Email</Link>
+    </div>
+</template>
+
+<script setup>
+    import {Link} from "@inertiajs/vue3";
+</script>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@ use App\Http\Controllers\RealtorListingAcceptOfferController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\UserAccountController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +34,21 @@ Route::get('login', [AuthController::class, 'create'])->name('login');
 Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
 
+Route::get('/email/verify',function () {
+    return inertia('Auth/VerifyEmail');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect()->route('listing.index')->with('success','Email was verified!');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('success', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 Route::resource('user-account', UserAccountController::class)->only(['create', 'store']);
 
 Route::resource('listing.offer', ListingOfferController::class)->middleware('auth')
@@ -42,7 +59,7 @@ Route::put('notification/{notification}/seen', NotificationSeenController::class
 
 Route::resource('notification', NotificationController::class)->middleware('auth')->only(['index']);
 
-Route::prefix('realtor')->name('realtor.')->middleware('auth')->group(function () {
+Route::prefix('realtor')->name('realtor.')->middleware(['auth','verified'])->group(function () {
     Route::name('listing.restore')->put('listing/{listing}/restore',
         [RealtorListingController::class, 'restore']
     )->withTrashed();
